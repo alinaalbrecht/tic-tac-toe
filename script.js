566eec83446571864bf42626ccc57db117d7c9ed
alinaalbrecht <alinaalbrecht@mailbox.org>
2021-04-25 17:37:02 +0200

    UI updates and let player input names

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const gameboard = (() => {
     let gameSquare = [...document.querySelectorAll(".game-square")];
+
     let gameboardValues = [
         "","","",
         "","","",
@@ -7,68 +8,76 @@ const gameboard = (() => {
         ];
 
     let updateGameboard = (index, value) => {
-        gameboardValues[index] = value;
-        
+        if (typeof(index) !== "string") {
+            for (let i = 0; i < gameboardValues.length; i++) {
+                gameboardValues[i] = "";
+            }
+            gameFlow.winnerFound = false;
+        }
+        else {
+            gameboardValues[index] = value;
+        }
         for (let i = 0; i < gameboardValues.length; i++) {
-        gameSquare[i].textContent = gameboardValues[i];}
+            gameSquare[i].textContent = gameboardValues[i];
+        }
         gameFlow.determineWinner(gameboardValues)
-
     }
-
-    return {
-        gameboardValues,
-        updateGameboard
-    }
+    return {gameboardValues, updateGameboard}
 })();
 
 const Player = (name, value) => {
     let play = () => {
         let currentVal = "";
         let triggered = false;
-        let gameSquare = [...document.querySelectorAll(".game-square")];
-        let availableSquare = gameSquare.filter(square => square.textContent === "");
-        availableSquare.forEach(square => square.addEventListener("click",
-        (e) => {
-            if (!triggered) {
-                triggered = true;
-                let index = e.target.getAttribute("data-cell");
-                currentVal = value
-                gameboard.updateGameboard(index, currentVal);
-                gameFlow.determineCurrent(currentVal);
-            }
-        }))
+        if (!gameFlow.determineWinner(gameboard.gameboardValues)) {    
+            let gameSquare = [...document.querySelectorAll(".game-square")];
+            let availableSquare = gameSquare.filter(square => square.textContent === "");
+            availableSquare.forEach(square => square.addEventListener("click",
+            (e) => {
+                if (!triggered && !gameFlow.determineWinner(gameboard.gameboardValues)) {
+                    /* triggered = true; */
+                    let index = e.target.getAttribute("data-cell");
+                    currentVal = value;
+                    gameboard.updateGameboard(index, currentVal);
+                    gameFlow.determineCurrent(currentVal);
+                }
+            }))
+        }
     }
     return {name, value, play}
 }
 
-const player1 = Player("nathaniel", "X")
-const player2 = Player("biene", "O")
-
 const gameFlow = (() => {
-    const determineCurrent = (val) => {
-    let currentPlayer = "";
-    
-    if (val === "X") {
-        currentPlayer = player2;
-    }
-    else if (val === "O") {
-        currentPlayer = player1;
-    }
-    else {
-        currentPlayer = "";
-    }
-    setTimeout(displayCurrent, 500, currentPlayer);
-    currentPlayer.play(currentPlayer);
+    const determineCurrent = (val = "X") => {
+        if (!gameFlow.determineWinner(gameboard.gameboardValues)) {
+            let currentPlayer = "";
+            if (val === "X") {
+                currentPlayer = player2;
+            }
+            else if (val === "O") {
+                currentPlayer = player1;
+            }
+            else {
+                currentPlayer = "";
+            }
+            setTimeout(displayCurrent, 500, currentPlayer);
+            currentPlayer.play();
+        }
+    };
+    const displayCurrent = (currentPlayer) => {
+        if (!gameFlow.determineWinner(gameboard.gameboardValues)) {
+            document.querySelector(".current-player").textContent = `current player: ${currentPlayer.name}`;
+        }
     };
 
-    const displayCurrent = (currentPlayer) => {
-        let currentPlayerName = currentPlayer.name;
-        let currentPlayerAlert = document.querySelector(".current-player")
-                            .textContent = `current player: ${currentPlayer.name}`;
-    };
+    let winnerFound = false;
 
     const determineWinner = (gameArray) => {
+        const player1name = player1.name;
+        const player2name = player2.name;
+        let winningPlayerAlert = document.querySelector(".current-player");
         let winningVal = "";
+
         //check rows
         if (gameArray[0] === gameArray[1] && gameArray[0] === gameArray[2]) {
             winningVal = gameArray[0];
@@ -101,20 +110,42 @@ const gameFlow = (() => {
 
         //check winner or tie
         if (winningVal === "X") {
-            console.log("player1 has won the game")
+            winningPlayerAlert.textContent = `${player1name} has won the game`; 
+            return winnerFound = true
         }
         else if (winningVal === "O") {
-            console.log("player2 has won the game")
+            winningPlayerAlert.textContent = `${player2name} has won the game`; 
+            return winnerFound = true
         }
         else if (gameArray.indexOf("") === -1 && winningVal === "") {
-            console.log("it's a tie")
+            winningPlayerAlert.textContent = "It's a Tie"; 
+            return winnerFound = true
         }
-    }
-    return {determineCurrent, displayCurrent, determineWinner}
+    }    
+    return {determineCurrent, displayCurrent, determineWinner, winnerFound}
 })();
 
-let startGame = (() => {
+const startGame = () => {
+    let player1Name = document.querySelector("#player-1-name").value;
+    let player2Name = document.querySelector("#player-2-name").value;
+    player1 = Player(player1Name, "X");
+    player2 = Player(player2Name, "O");
+    let nameFields = document.querySelectorAll("input");
+    nameFields.forEach(input => input.value = "");
+    document.querySelector("form").classList.add("hide");
+    document.querySelector(".restart-button").classList.remove("hide");
     let currentPlayer = player1;
     currentPlayer.play();
     gameFlow.displayCurrent(currentPlayer)
-})();
+    gameFlow.determineWinner([], player1, player2)
+};
+
+let submit = document.querySelector(".submit-player-names");
+submit.addEventListener("click", startGame);
+
+let restart = document.querySelector(".restart-button");
+restart.addEventListener("click", gameboard.updateGameboard);
+
+
+
+
